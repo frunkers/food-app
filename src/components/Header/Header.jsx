d1475ffe87233c645d0090b85ba560d556aa95ce
Dmitry <dimaruzaev2@gmai.com>
2022-12-30 20:52:25 +0300

    !change menu and add search in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,13 +2,7 @@ import { Component } from 'react';
 import "./Header.css";
 
 export default class Header extends Component {
-	constructor(props) {
-		super(props);
-	}
-
 	render() {
-		const { avatar } = this.props;
-
 		return (
 			<header className="header">
 				<div className="header__container">
@@ -20,8 +14,13 @@ export default class Header extends Component {
 						</div>
 					</div>
 
-					<div className="avatar header__el" tabIndex="0">
-						<img src={avatar} alt="avatar" className="avatar__icon" />
+					<div className="header__el header__el_search">
+						<form className="header__form">
+							<label htmlFor="search" className="header__label">
+								<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none"><circle cx="11.77" cy="11.77" r="8.99" stroke="#AAACAE" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"/><path stroke="#AAACAE" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M18.02 18.49 21.54 22"/></svg>
+								<input className="header__search" placeholder='Search' id="search"></input>
+							</label>
+						</form>
 					</div>
 				</div>
 			</header>
--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -9,6 +9,13 @@ import notificationActive from "../../assets/img/icons/notificationActive.svg";
 import buy from "../../assets/img/icons/buy.svg";
 import buyActive from "../../assets/img/icons/buyActive.svg";
 
+const menuIcons = [
+	{default: home, active: homeActive, alt: "home"},
+	{default: like, active: likeActive, alt: "like"},
+	{default: notification, active: notificationActive, alt: "notification"},
+	{default: buy, active: buyActive, alt: "buy"},
+];
+
 export default class Menu extends Component {
 	constructor() {
 		super();
@@ -31,29 +38,15 @@ export default class Menu extends Component {
 				<div className="menu__container">
 					<nav className="menu-wrap">
 						<ul className="menu__list">
-							<li onClick={() => this.handleActive(0)} className="menu__item" tabIndex="0">
-								<img src={isActive === 0 ? homeActive : home} alt="home" className="menu__icon" />
-							</li>
-
-							<li onClick={() => this.handleActive(1)} className="menu__item" tabIndex="0">
-								<img src={isActive === 1 ? likeActive : like} alt="like" className="menu__icon" />
-							</li>
-
-							<li className="menu__item" tabIndex="0">
-								<div className="menu__search">
-									<div className="menu__search-shadow">
-										<svg className="menu__icon menu__icon_search" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 60 60"><circle cx="30" cy="30" r="30" fill="url(#a)"/><circle cx="29.77" cy="29.77" r="8.99" stroke="#fff" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5"/><path stroke="#fff" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M36.02 36.49 39.54 40"/><defs><linearGradient id="a" x1="25.16" x2="65.24" y1="-18" y2="-4.41" gradientUnits="userSpaceOnUse"><stop stopColor="#F9881F"/><stop offset="1" stopColor="#FF774C"/></linearGradient></defs></svg>
-									</div>
-								</div>
-							</li>
-
-							<li onClick={() => this.handleActive(3)} className="menu__item" tabIndex="0">
-								<img src={isActive === 3 ? notificationActive : notification} alt="notification" className="menu__icon" />
-							</li>
-
-							<li onClick={() => this.handleActive(4)} className="menu__item" tabIndex="0">
-								<img src={isActive === 4 ? buyActive : buy} alt="buy" className="menu__icon" />
-							</li>
+							{
+								menuIcons.map((el, index) => {
+									return (
+										<li onClick={() => this.handleActive(index)} className="menu__item" tabIndex="0" key={el.alt}>
+											<img src={isActive === index ? el.active : el.default} alt={el.alt} className="menu__icon" />
+										</li>
+									)
+								})
+							}
 						</ul>
 					</nav>
 				</div>
